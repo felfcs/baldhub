c78dede250a2cfdb0bf7ee6365cf47aa11e00c34
felfcs <felfcs@hotmail.com>
2018-05-16 23:21:46 +0200

    teste

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+
+		<title>baldhub</title>
+
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+		<link rel="stylesheet" type="text/css" href="mystyle.css">
+
+		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+
+		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+
+		<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+
+		<!-- <script src="script.js"></script>	-->
+
+	</head>
+
+	<div>ffff</div>
+
+	<body>	
+
+		<header>
+
+			<div class="header">
+
+			<div class="topo">			 	
+
+				<div class="logo_pequeno">
+		
+					<img src="/baldhub/images/logo-calva-peq.png" alt="baldhub">
+			
+				</div>
+
+  				<div class="navbar">
+
+  					<a href="#">about us</a>
+  					<a href="#">cities</a>
+  					<a href="#">advantages</a>
+  					<a href="#">blog</a>
+  					<a href="#">social</a>
+  					<a href="#">contact</a>
+
+				</div>
+
+			</div>
+
+			<div class="logo_grande">
+
+			</div>
+
+			</div>
+	
+		</header>
+
+	</body>
+
+</html> 
+
